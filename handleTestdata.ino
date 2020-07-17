59958cb8fe2c8223aa6af79aedd76ceea1ff8e66
Robert van den Breemen <robert@vandenbreemen.net>
2020-07-17 13:55:48 +0200

    before update 2.1.0-rc0

--- handleTestdata.ino
+++ handleTestdata.ino
@@ -1,4 +1,13 @@
-
+/* 
+***************************************************************************  
+**  Program  : handleTestdata.ino, part of DSMRloggerAPI
+**  Version  : v2.1.0-rc0
+**
+**  Copyright (c) 2020 Willem Aandewiel
+**
+**  TERMS OF USE: MIT License. See bottom of file.                                                            
+***************************************************************************      
+*/
 
 #if defined(HAS_NO_SLIMMEMETER)
 
@@ -512,3 +521,27 @@ unsigned int CRC16(unsigned int crc, unsigned char *buf, int len)
 }
 
 #endif
+
+
+/***************************************************************************
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to permit
+* persons to whom the Software is furnished to do so, subject to the
+* following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+* OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+* THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+***************************************************************************/
