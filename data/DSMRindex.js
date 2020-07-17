3272654361797af6027a52aa70e86245cd87849d
Robert van den Breemen <robert@vandenbreemen.net>
2020-07-17 13:59:10 +0200

    update version to 2.1.0-rc0

--- data/DSMRindex.js
+++ data/DSMRindex.js
@@ -1,7 +1,7 @@
 /*
 ***************************************************************************  
 **  Program  : DSMRindex.js, part of DSMRfirmwareAPI
-**  Version  : v2.1.0-rc0-rc0
+**  Version  : v2.1.0-rc0
 **
 **  Copyright (c) 2020 Willem Aandewiel
 **
