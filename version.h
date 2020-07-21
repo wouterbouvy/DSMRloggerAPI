9af0cce5e846ef25f0907db47b2fd5984dc8ad6c
Robert van den Breemen <robert@vandenbreemen.net>
2020-07-21 22:30:53 +0200

    fixed update on load

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 2
 #define _VERSION_MINOR 1  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 115
+#define _VERSION_BUILD 116
 #define _VERSION_PRERELEASE rc0
 #define _VERSION_DATE "21-07-2020"
-#define _VERSION_TIME "17:03:20"
+#define _VERSION_TIME "22:04:21"
 #define _SEMVER_CORE "2.1.0"
-#define _SEMVER_BUILD "2.1.0+115"
-#define _SEMVER_FULL "2.1.0-rc0+115"
+#define _SEMVER_BUILD "2.1.0+116"
+#define _SEMVER_FULL "2.1.0-rc0+116"
 #define _SEMVER_NOBUILD "2.1.0-rc0 (21-07-2020)"
-#define _VERSION "2.1.0-rc0+115 (21-07-2020)"
+#define _VERSION "2.1.0-rc0+116 (21-07-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
