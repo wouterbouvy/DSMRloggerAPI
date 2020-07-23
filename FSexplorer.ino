c2ac3d74bd1bb912e537e51dd340524fc275bb85
Robert van den Breemen <robert@vandenbreemen.net>
2020-07-23 16:21:25 +0200

    refactoring remove string-casting

--- FSexplorer.ino
+++ FSexplorer.ino
@@ -59,16 +59,15 @@ void setupFSexplorer()    // Funktionsaufruf "spiffs();" muss im Setup eingebund
   httpServer.on("/update", updateFirmware);
   httpServer.onNotFound([]() 
   {
-    if (Verbose2) DebugTf("in 'onNotFound()'!! [%s] => \r\n", String(httpServer.uri()).c_str());
+    if (Verbose2) DebugTf("in 'onNotFound()'!! [%s] => \r\n", httpServer.uri().c_str());
     if (httpServer.uri().indexOf("/api/") == 0) 
     {
-      if (Verbose1) DebugTf("next: processAPI(%s)\r\n", String(httpServer.uri()).c_str());
+      if (Verbose1) DebugTf("next: processAPI(%s)\r\n", httpServer.uri().c_str());
       processAPI();
     }
     else
     {
-      DebugTf("next: handleFile(%s)\r\n"
-                      , String(httpServer.urlDecode(httpServer.uri())).c_str());
+      DebugTf("next: handleFile(%s)\r\n", httpServer.urlDecode(httpServer.uri()).c_str());
       if (!handleFile(httpServer.urlDecode(httpServer.uri())))
       {
         httpServer.send(404, "text/plain", "FileNotFound\r\n");
--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 2
 #define _VERSION_MINOR 1  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 166
+#define _VERSION_BUILD 169
 #define _VERSION_PRERELEASE rc0
 #define _VERSION_DATE "23-07-2020"
-#define _VERSION_TIME "11:19:38"
+#define _VERSION_TIME "16:12:46"
 #define _SEMVER_CORE "2.1.0"
-#define _SEMVER_BUILD "2.1.0+166"
-#define _SEMVER_FULL "2.1.0-rc0+166"
+#define _SEMVER_BUILD "2.1.0+169"
+#define _SEMVER_FULL "2.1.0-rc0+169"
 #define _SEMVER_NOBUILD "2.1.0-rc0 (23-07-2020)"
-#define _VERSION "2.1.0-rc0+166 (23-07-2020)"
+#define _VERSION "2.1.0-rc0+169 (23-07-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
