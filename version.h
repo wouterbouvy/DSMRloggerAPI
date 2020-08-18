c5d0e8099d882b1569fbde95f52519131327012e
Robert van den Breemen <robert@vandenbreemen.net>
2020-08-18 23:04:15 +0200

    before update 2.1.2-rc2

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #define _VERSION_MINOR 1  
 #define _VERSION_PATCH 2
 #define _VERSION_BUILD 326
-#define _VERSION_PRERELEASE rc1
+#define _VERSION_PRERELEASE rc2
 #define _VERSION_DATE "18-08-2020"
 #define _VERSION_TIME "23:02:14"
 #define _SEMVER_CORE "2.1.2"
